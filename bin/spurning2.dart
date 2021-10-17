64758f94630466a46f0d381c79fe863b931b7f38
Feykir <kollit90@gmail.com>
2021-10-17 12:13:33 +0000

    Spurning 2

---
+++ bin/spurning2.dart
@@ -0,0 +1,15 @@
+
+
+void main(List<String> arguments) {
+
+  int counter = 0;
+  List<int> calculateSum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+
+  for(int numbers in calculateSum){
+    if(numbers >= 20 && numbers <= 80){
+      counter++;
+      print(numbers);
+    }
+  }
+  print('There are $counter between 20 and 80');
+}
