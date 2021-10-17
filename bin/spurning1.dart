2412564700e3c7b00da0efd468e7123d564fc91e
Feykir <kollit90@gmail.com>
2021-10-17 12:02:47 +0000

    pupspec og spurning 1

---
+++ bin/spurning1.dart
@@ -0,0 +1,12 @@
+
+
+void main(List<String> arguments) {
+
+  int sum = 0;
+  List<int> calculateSum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+
+  for (int i = 0; i < calculateSum.length; i++){
+    sum += calculateSum[i];
+  }
+  print('Sum = $sum');
+}
