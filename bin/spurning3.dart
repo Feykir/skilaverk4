c26bcccb977a22179369c6f6d5c962da2e51156c
Feykir <kollit90@gmail.com>
2021-10-17 14:50:24 +0000

    Spurning 3 vesen

--- bin/spurning3.dart
+++ bin/spurning3.dart
@@ -3,23 +3,33 @@ import 'dart:io';
 import 'dart:math';
 
 
-void main (){
-
+void main () {
   List<int> numberList = [];
 
   bool askUser = true;
-  while(askUser){
+  while (askUser) {
     print('Please enter a number:');
     String userInput = stdin.readLineSync();
     int userNumbers = int.parse(userInput);
     numberList.add(userNumbers);
 
-    if(userInput == ''){
-      break;
+    ['yes', if(userInput == "") 'no'];
+    /* Mér var bent á að nota getYesNo functionið, var ekki alveg viss hvernig ég átti að implementa það.
+    Þegar ég setti þetta upp svona og setti print statementin fyrir neðan inní while lykkjuna þá virkaði þetta,
+    en það prentaði út listann alltaf þegar ég sló inn tölu en fékk samt alltaf sömu villu þegar ég sló bara inn Enter.
+    Viðurkenni að ég hef ekki hugmynd um hvað ég ætti að gera til að leysa þetta.
+     */
 
-    }
+    /*if(userInput == ''){
+    break;
+    Þetta virkaði ekki neitt, fékk alltaf "FormatException: Invalid number (at character 1)" villu
+  alveg sama hvað ég gerði í if statement.
+  Grunar að þetta hafi eitthvað að gera með að ég er að nota int sem variable en hef ekki hugmynd um hvernig ég laga það.
+
+    }*/
+
+
   }
-  print(numberList.reduce(max));
   print(numberList.reduce(min));
-
+  print(numberList.reduce(max));
 }
